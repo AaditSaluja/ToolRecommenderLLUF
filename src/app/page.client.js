85b1ce25c3320f8415b4a2fe20f1ec5e78395d8b
Aadit Saluja <saluja_aadit@outlook.com>
2023-11-06 15:57:43 -0500

    Mostly Working

--- src/app/page.client.js
+++ src/app/page.client.js
@@ -1,14 +1,35 @@
 "use client"; 
 import { useState, useEffect } from 'react';
 import DisplayData from './Button.js';
+import Alert from "./Alert.js";
 import OpenAI from "openai";
 
+function WaitingScreen() {
+    return <div>Waiting for GPT response...</div>;
+}
+  
+function ResultScreen({ result }) {
+    console.log(result.message.content);
+    return (
+        <div style = {{
+            padding: "10px",
+            margin: "10px",
+            color: "#2F5858",
+            width: "40vw",
+          }}>
+        <p> {result.message.content}</p>
+        </div>
+    );
+    
+}
 
 
 function CheckData({ data, apkey}) {
     const [pressedButtons, setPressedButtons] =  useState({});
     const [activeButton, setActiveButton] = useState(null);
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [gptResponse, setGptResponse] = useState(null);
 
     const handleButtonClick = (key, value) => {
         const alreadyPressedButtons = {...pressedButtons};
@@ -19,60 +40,38 @@ function CheckData({ data, apkey}) {
             alreadyPressedButtons[key] = value;
         }
         setPressedButtons(alreadyPressedButtons);
-        // setPressedButtons.key = value;
-        // setPressedButtons(prevButtons => ({...prevButtons, key: value }));
         setActiveButton(key);        
-        // console.log(key);
-        // console.log(alreadyPressedButtons[key]);
+
     
     }
     const sendToGPT = async () => {
-        // const apiEndpoint = 'https://api.openai.com/v1/chat/completions';
-        // const requestOptions = {
-        //     method: 'POST',
-        //     headers: {
-        //       Authorization: `Bearer ${apiKey}`,
-        //       'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //       prompt: 'HIYAYAY',
-        //       max_tokens: 50,
-        //     }),
-        //  };
-        
-        //   try {
-        //     const response = await fetch(apiEndpoint, requestOptions);
-        
-        //     if (response.ok) {
-        //       const data = await response.json();
-        //       const generatedText = data.choices[0].text;
-        //       console.log(generatedText);
-        //     } else {
-        //       // Handle API request errors here
-        //       console.error('API request failed');
-        //     }
-        //   } catch (error) {
-        //     // Handle network or other errors here
-        //     console.error('API request error:', error);
-        //   }
-        
         let message = "Hi, I want you to help me decide what tool I would enjoy learning the most out of Adobe Illustrator, Adobe Photoshop, Canva, React/Next JS, Blender, Adobe Premiere, and any tool of your choice based on some of things I like. Answer briefly in 2-3 lines.  ";
-        message += "I like" + pressedButtons[0] + ", " + pressedButtons[1] + ", " + pressedButtons[2] + ", " + pressedButtons[3] + ", " + pressedButtons[4] + ", " + pressedButtons[5] + ", " + pressedButtons[6] + ", " + pressedButtons[7] + ", ";
-        console.log(message);
+        for(let i = 0; i < data.length; i++){
+            if(pressedButtons[i] == null){
+                console.log("Please select all options");
+                return;
+            }
+            message += "I prefer " + pressedButtons[i] + " between " + data[i][Object.keys(data[i])[0]] + ' and ' + data[i][Object.keys(data[i])[1]] + ". ";
+        }
+
+        setIsLoading(true);
+        
         const openai = new OpenAI({apiKey: apkey, dangerouslyAllowBrowser: true});
         const completion = await openai.chat.completions.create({
           messages: [{ role: "system", content: message }],
           model: "gpt-4",
         });
       
-        console.log(completion.choices[0]);
+        setGptResponse(completion.choices[0]);
+        setIsLoading(false);
     }
     
     return (
         
         <div>
-            <DisplayData data={data} onButtonClick={handleButtonClick} pressedButtons={pressedButtons} />
-            <button className='submit-button' onClick={sendToGPT}>Submit</button>
+            
+            {isLoading ? <WaitingScreen /> : (gptResponse ? <ResultScreen result={gptResponse} /> : <DisplayData data={data} onButtonClick={handleButtonClick} pressedButtons={pressedButtons} /> )}
+            {isLoading ? null: (gptResponse ? null : <button className='submit-button' onClick={sendToGPT}>Submit</button>)}
         </div>
     )
     ;
